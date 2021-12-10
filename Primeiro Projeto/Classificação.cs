f2288f5c3ed4a205fc4053e191ee974beeae179d
Fellipe Augusto <Fellipe246@hotmail.com>
2021-12-10 20:15:52 -0300

    Pessoa C#

---
+++ Primeiro Projeto/Classificação.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CShaarp
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public string idade { get; set; }
+        public string etnia { get; set; }
+    }
+}
--- Primeiro Projeto/program.cs
+++ Primeiro Projeto/program.cs
@@ -1,11 +1,19 @@
 using System;
-
+using CShaarp;
+{
+     
+}
 namespace Teste
 {
     class program
     {
         static void main(string[]args)
         {
+            Pessoa Person = new Pessoa();
+            Person.Nome = "Fellipe";
+            Person.idade = "21 anos";
+            Person.etnia = "Branco";
+
             Console.WriteLine("Hello World");
         }
     }
