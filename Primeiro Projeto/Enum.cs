c090c75f38be3b79568f4202264764ac1d5c0c1c
Fellipe Augusto <Fellipe246@hotmail.com>
2021-12-11 07:56:13 -0300

    Estruturas

---
+++ Primeiro Projeto/Enum.cs
@@ -0,0 +1,10 @@
+namespace Enum
+{
+    enum Pessoa
+    {   
+        
+
+
+    }
+        
+}
---
+++ Primeiro Projeto/Interface.cs
@@ -0,0 +1,31 @@
+using System;
+using System text;
+
+namespace Interface
+{
+    interface IAnimal
+    {
+        void nome (String nome);
+        void Dono(String nomeDono);
+        Void Especie(String Especie);
+    }
+          class Animal : IAnimal
+          {
+                public String Nome { get; set; } 
+                public string NomeDono { get; set; } 
+                public String Especie { get; set; }
+
+                void IAnimal.nome(string Nome)
+                {
+                    this.Nome = Nome;
+                }
+                void IAnimal.Dono(string nomeDono)
+                {
+                    this.NomeDono = nomeDono;
+                }    
+                void IAnimal.Especie(string Especie)
+                {
+                    this.Especie = Especie; 
+                }
+          }  
+}
