32c6a3746613dc678e3fa981702b68cc1e69107d
Fellipe Augusto <Fellipe246@hotmail.com>
2021-12-12 14:49:07 -0300

    C#

--- Primeiro Projeto/Classes.cs
+++ Primeiro Projeto/Classes.cs
@@ -5,5 +5,7 @@ namespace CSharp
     class Pessoa
     {
         public string nome { get; set; }
+        public int idade { get; set; }
+        public string cidade { get; set; }
     }
 }
--- Primeiro Projeto/Final.cs
+++ Primeiro Projeto/Final.cs
@@ -1,5 +1,6 @@
 using System;
 using Primeiro;
+using CSharp;
 
 namespace CSharp
 {
@@ -7,6 +8,18 @@ namespace CSharp
     {
         static void main(string[] args)
         {
+            Pessoa person = new Pessoa();
+
+            pessoa.nome = "Fellipe";
+            person.idade = 21;
+            person.cidade = "Olinda"
+
+            var person2 = new pessoa();
+
+            Pessoa2.nome = "Leticia";
+            Pessoa2.idade = 25;
+            Pessoa2.cidade = "camaragibe"
+
             var Classe = new Classe();
             var Classe2 = new Segundo.classe();
         
