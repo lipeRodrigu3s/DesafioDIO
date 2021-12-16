e35bbed0744b46d23effa268bd62e869413d2f66
Fellipe Augusto <Fellipe246@hotmail.com>
2021-12-16 20:01:49 -0300

    Error CS0103

--- Primeiro Projeto/.Net/ProjetoDIO/Program.cs
+++ Primeiro Projeto/.Net/ProjetoDIO/Program.cs
@@ -1,20 +1,18 @@
-﻿namespace ProjetoDIO
-{
-    class program
-    {
-        static void main(string[] args)
-             
-        {
-            int numeroDevezes = 10;
+﻿using System;
 
-            for (int i=0; i<numeroDevezes; i++)
-            {
-                console.writeline("Meus primeiros passos em .Net {i} ");
-            }
-                
-    
+namespace ProjetoDio
+{
+    class ProjetoDio
+    {
+        static void Main(string[] args)
+        {
+            int NumeroRepeticao = 10;
+            
+            for (int i = 0; i < NumeroRepeticao; i++)
+            
+              console.writeline("Meus primeiros passos em .Net {i} ");
+            
         }
     }
 }
-
 
