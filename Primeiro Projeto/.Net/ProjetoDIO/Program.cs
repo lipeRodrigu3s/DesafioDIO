a75d0891ae16d4f9f9fe08a508e050cba0cc607d
Fellipe Augusto <Fellipe246@hotmail.com>
2021-12-16 19:38:14 -0300

    .Net

--- Primeiro Projeto/.Net/ProjetoDIO/Program.cs
+++ Primeiro Projeto/.Net/ProjetoDIO/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ProjetoDIO
+{
+    class program
+    {
+        static void main(string[] args)
+             
+        {
+            int numeroDevezes = 10;
+
+            for (int i=0; i<numeroDevezes; i++)
+            {
+                console.writeline("Meus primeiros passos em .Net {i} ");
+            }
+                
+    
+        }
+    }
+}
+
+
