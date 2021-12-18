91ff457cfc3399e612801573e1aa7c7264a7d7f1
Fellipe Augusto <Fellipe246@hotmail.com>
2021-12-18 09:06:05 -0300

    Contador C#

---
+++ Primeiro Projeto/C#/Contador.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace contador
+{
+    class program
+    {
+        static void contadorif(string[] args)
+        {
+            Console.ReadLine();
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Nenhum argumento");
+            }
+            else if (args.Length == 1)
+            {
+                Console.WriteLine("temos um argumento");
+            }
+            else
+            {
+                Console.WriteLine($"{args.Length} argumentos");
+            }
+
+        }
+    }
+}    
