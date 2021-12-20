2a7a2c6971375a146880f985868eedc6c613df3a
Fellipe Augusto <Fellipe246@hotmail.com>
2021-12-20 19:21:58 -0300

    Pratica Ponto C#

---
+++ Primeiro Projeto/C#/Ponto.cs
@@ -0,0 +1,22 @@
+namespace Classes.herança
+{
+    public class ponto
+    {
+        public int x, y;
+
+        private int distancia;
+
+
+        public ponto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+
+        protected void calculardistancia()
+        {
+            
+        }
+    }
+}
