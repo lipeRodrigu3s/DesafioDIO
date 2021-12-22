e5325e9b7a7ba6d9e0ce8185240e3807835998fa
Fellipe Augusto <Fellipe246@hotmail.com>
2021-12-21 21:01:34 -0300

    Revisão C#

---
+++ Primeiro Projeto/.Net/RevisaoC#/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha uma das opções abaixo");
+            Console.WriteLine("1- Cadastrar novo Aluno");
+            Console.WriteLine("2- Lista de Alunos");
+            Console.WriteLine("3- Calcular media geral");
+            Console.WriteLine("4- Sair");
+
+            Console.WriteLine();
+
+            string EscolhaUsuario = Console.ReadLine();
+
+            while (EscolhaUsuario.ToUpper() != "4")
+            {
+                switch(EscolhaUsuario)
+                {
+                    case "1":
+
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "3":
+
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();            
+                }
+            }
+        }
+    }
+}
