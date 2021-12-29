4a86c997590df94f8ac4a6e49d366a16624148db
Fellipe Augusto <Fellipe246@hotmail.com>
2021-12-29 18:59:42 -0300

    Update Program.cs

--- Primeiro Projeto/POO/ProjetoPOO/Program.cs
+++ Primeiro Projeto/POO/ProjetoPOO/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ProjetoPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.nome = "Lipe";
+            p1.idade = 21;
+            p1.apresentar();
+        }
+    }
+}
