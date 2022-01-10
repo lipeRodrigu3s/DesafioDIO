1f438057bdf64f3d69900296b80626d36eed2065
Fellipe Augusto <Fellipe246@hotmail.com>
2022-01-10 19:13:40 -0300

    C#

---
+++ Primeiro Projeto/Construtures/ExemploConstrutores/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class program
+    {
+        public void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Fellipe", "Augusto");
+            p1.Apresentar();
+        }
+    }
+}
