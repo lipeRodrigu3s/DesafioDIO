5f83d2067acb702a1eec0047c90e999b00a92d6f
Fellipe Augusto <Fellipe246@hotmail.com>
2022-01-13 14:49:59 -0300

    Arrays

---
+++ Primeiro Projeto/Arrays/Colecoes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Colecoes
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayInteiros = new int[5]
+    
+        }
+    }
+}
