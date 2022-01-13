2c4a7756210683ec4e3442810d2499130095d171
Fellipe Augusto <Fellipe246@hotmail.com>
2022-01-13 15:05:50 -0300

    Arrays

--- Primeiro Projeto/Arrays/Colecoes/Program.cs
+++ Primeiro Projeto/Arrays/Colecoes/Program.cs
@@ -6,8 +6,25 @@ namespace Colecoes
     {
         static void Main(string[] args)
         {
-            int[] arrayInteiros = new int[5]
-    
+            int[] arrayInteiros = new int[5];
+
+            arrayInteiros[0] = 5;
+            arrayInteiros[1] = 4;
+            arrayInteiros[2] = 7;
+            arrayInteiros[3] = 10;
+            arrayInteiros[4] = 80;
+            
+            System.Console.WriteLine("Trilhando o array pelo for");
+
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                System.Console.WriteLine(arrayInteiros[i]);
+            }
+            System.Console.WriteLine("Trilhando o array pelo foreach");
+            foreach (int item in arrayInteiros)
+            {
+                System.Console.WriteLine(item);
+            }
         }
     }
 }
