f649990f16746a611ebbb52de390a863547ea941
Fellipe Augusto <Fellipe246@hotmail.com>
2022-01-20 09:45:50 -0300

    APP.DIO

--- Primeiro Projeto/PROJETO/APPDIO/Program.cs
+++ Primeiro Projeto/PROJETO/APPDIO/Program.cs
@@ -10,5 +10,39 @@ namespace APPDIO
 
             System.Console.WriteLine("Hello World");
         }   
+
+        private static string ObterOpcaoCliente()
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("App da Dio sempre aqui por voce!");
+            System.Console.WriteLine("Informe a melhor opcao pra voce: ");
+
+            System.Console.WriteLine("Listar Catalogo de Codigos");
+            System.Console.WriteLine("1, Mostrar Codigos");
+            System.Console.WriteLine("2, Inserir novo Codigo");
+            System.Console.WriteLine("3, Atualizar Codigo ");
+            System.Console.WriteLine("4, Excluir Codigo");
+            System.Console.WriteLine("5, Visualizar Codigo");
+            System.Console.WriteLine("C, Limpar Area ");
+            System.Console.WriteLine("X, Ate mais! ");
+
+            string ObterOpcaoCliente = Console.Readline().ToUpper();
+            System.Console.WriteLine();
+            return ObterOpcaoCliente;
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
