eafd80b506951a48119f0243f59c68d703437bf3
Fellipe Augusto <Fellipe246@hotmail.com>
2022-02-17 20:51:41 -0300

    SomaPar

---
+++ Segundo Projeto/SomarPar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System; 
+
+namespace SomarPar
+{
+    class program   
+    {
+        static void Main()
+        {   
+            
+            int x;
+            Console.WriteLine("informe o numero: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            for (int n = 1; n <= 5; n++)
+            {
+                if (x % 2 == 0)
+                {
+                   do
+                   {
+                       x = x + 2;
+                       Console.WriteLine(x);
+                   } 
+                   while (n <= 5);
+
+                   
+                }
+            }
+        }
+    }
+}
