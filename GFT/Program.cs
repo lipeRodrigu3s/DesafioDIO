b8be19b7898461cbf708712f0d1db275c09b0a53
Fellipe Augusto <Fellipe246@hotmail.com>
2022-02-17 21:52:21 -0300

    FileC#

--- GFT/Program.cs
+++ GFT/Program.cs
@@ -2,15 +2,43 @@
 
 System.Console.WriteLine("Digete o nome do arquivo: ");
 var Name = Console.ReadLine();
+Name = CleanName(Name);
+
+foreach (var @char in Path.GetInvalidFileNameChars())
+{
+    Name = Name.Replace(@char, '*');
+}
+
 var path = Path.Combine(Environment.CurrentDirectory, $"{Name}.txt");
 
 NewFile(path);
 
+System.Console.WriteLine("Pressiona enter para finalizar");
+Console.ReadLine();
+
+static string CleanName(string Name)
+{
+    foreach (var @char in Path.GetInvalidFileNameChars())
+    {
+        Name = Name.Replace(@char, '*');
+    }
+        return Name;
+}
+
 static void NewFile(string Path)
 {
-    var Tx = File.CreateText(Path);
-    Tx.WriteLine("Primeiro arquivo criado");
-    Tx.Flush();
+    try
+    {
+        var Tx = File.CreateText(Path);
+        Tx.WriteLine("Primeiro arquivo criado");
+        Tx.Flush();
+    }
+    catch 
+    {
+        System.Console.WriteLine("Nome de Arquio invalido");
+    }
+
+   
 }
 
 
